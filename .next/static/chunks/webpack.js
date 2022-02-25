280713247827d79d1f82fec7318343fbc388d3f3
Katarina Lea <katarina.lea@globacore.com>
2022-02-25 11:29:49 -0500

    still not having any luck

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4571d944216e71b7"; }
+/******/ 		__webpack_require__.h = function() { return "bba0fd662517f96b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Box, Text } from 'theme-ui'
 import Row from '../components/Row'
 
+
 export default function Home({ users }) {
 
     return (
@@ -49,18 +50,18 @@ export const getStaticProps = async () => {
     const leaderboard = await fetch("https://scorebot-api-service-q3nu3.ondigitalocean.app/v1/leaderboards/PP3SPA6hmE89dX-ZLq-s9/entries?page=1&size=50");
     const data = await leaderboard.json()
 
-    require('dotenv').config();
     const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
     const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
     const client = require('twilio')(accountSid, authToken);
 
     client.messages
-    .create({
-        body: 'Hello from TWILIO!',
-        to: '+12899280315', // Text this number
-        from: '+12163696199', // From a valid Twilio number
-    })
-    .then((message) => console.log(message.sid));
+        .create({
+            body: 'Hello from TWILIO!',
+            to: '+12899280315', // Text this number
+            from: '+12163696199', // From a valid Twilio number
+
+        })
+        .then(message => console.log(message.sid));
 
     return {
         props: {
